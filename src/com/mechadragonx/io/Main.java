de3e5478b8c2283ac0bdd224cf930cb0bc84fd48
MechaDragonX <sonicraghav@gmail.com>
2019-10-24 17:32:41 -0700

    Reads all bytes in the text file and coverts them to 32-bit ints

--- src/com/mechadragonx/io/Main.java
+++ src/com/mechadragonx/io/Main.java
@@ -1,10 +1,62 @@
 package com.mechadragonx.io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.Files.newBufferedReader;
+
 public class Main
 {
+    private static final int[] POWERS_OF_10 = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        
+        String path = "./data/random.txt";
+        long start = System.currentTimeMillis();
+        readAsBytes(path);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+    private static ArrayList<Integer> readAsBytes(String path) throws IOException
+    {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int start = 0;
+        int end;
+        int num = 0;
+        for(int i = 0; i < bytes.length; i++)
+        {
+            if((bytes[i] == 13 && bytes[i + 1] == 10) || (i == bytes.length - 1))
+            {
+                if(i != bytes.length - 1)  end = i - 1;
+                else end = i;
+                for(int j = end; j >= start; j--)
+                {
+                    num += ((bytes[j] - 48) * powerOfTen(end - j));
+                }
+                numbers.add(num);
+                num = 0;
+                start = i + 2;
+                i++;
+            }
+        }
+        return numbers;
+    }
+    private static int powerOfTen(int pow)
+    {
+        return POWERS_OF_10[pow];
+    }
+    private static void printCollection(ArrayList<Integer> list)
+    {
+        for(int num : list)
+        {
+            System.out.print(num + "\t");
+        }
     }
 }
