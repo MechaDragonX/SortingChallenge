508f9903f61e56a2142e4aba497ea218d0446e41
MechaDragonX <sonicraghav@gmail.com>
2019-10-31 14:06:45 -0700

    Minor changes and removed modified file.

--- src/com/mechadragonx/io/Main.java
+++ src/com/mechadragonx/io/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main
